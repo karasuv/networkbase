1f00fd48d0bad201b348e080bd10350d36a5431a
karasuv <karasuv@inbox.ru>
2021-10-21 19:43:49 +0300

    болванка для итогового проекта, извиняюсь что не успеваю сдавать, но были домашние проблемы, буду стараться в ближайшие дни допилить до чего-нибудь что можно будет проверить)

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,8 +1,26 @@
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
 public class Client {
     public static void main(String[] args) {
 
     }
 
+        public void start(){
+            NioEventLoopGroup group = new NioEventLoopGroup();
+            Bootstrap client = new Bootstrap();
+            client.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel ch) throws Exception {
+                            ch.pipeline().addLast()
+                        }
+                    })
+        }
 
 
 }
--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,5 +1,32 @@
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class Server {
     public static void main(String[] args) {
-        
+
     }
+
+    public void start(){
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+
+                        }
+                    })
+        }
+
+
+    }
+
+
 }
